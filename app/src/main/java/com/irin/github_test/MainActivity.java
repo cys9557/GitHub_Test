89305638e1812eb5cdf3e011f45f010a046fe28c
cys9557 <47514206+cys9557@users.noreply.github.com>
2019-02-11 15:58:00 +0900

    Update MainActivity.java

--- app/src/main/java/com/irin/github_test/MainActivity.java
+++ app/src/main/java/com/irin/github_test/MainActivity.java
@@ -11,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         getSupportActionBar().setTitle("Nice to meet you");
+        
+        //GitHub에서 데이터가 변경...
     }
 }
